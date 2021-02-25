b40678090657643c503a2e6ed4e52098cf128ba8
Max Ghenis <mghenis@gmail.com>
2021-02-24 19:36:40 -0800

    Fix docstring

--- ubicenter/logo.py
+++ ubicenter/logo.py
@@ -8,7 +8,7 @@ def add_ubi_center_logo(fig: go.Figure, x: float, y: float = -0.12) -> None:
     :type fig: go.Figure
     :param x: Horizontal coordinate.
     :type x: float
-    :param y: Horizontal coordinate, defaults to -0.12.
+    :param y: Vertical coordinate, defaults to -0.12.
     :type y: float, optional
     """
     fig.add_layout_image(
