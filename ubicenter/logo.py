8d4a55d81ef205cb718e958ed664c109fba2dd56
Max Ghenis <mghenis@gmail.com>
2021-02-24 19:47:33 -0800

    Add format_fig

--- ubicenter/logo.py
+++ ubicenter/logo.py
@@ -1,7 +1,11 @@
 from plotly import graph_objects as go
 
 
-def add_ubi_center_logo(fig: go.Figure, x: float, y: float = -0.12) -> None:
+CONFIG = {"displayModeBar": False}
+
+def add_ubi_center_logo(
+    fig: go.Figure, x: float = 0.98, y: float = -0.12
+) -> None:
     """ Adds UBI Center logo to a plotly figure. Returns nothing.
 
     :param fig: Plotly figure.
@@ -26,3 +30,35 @@ def add_ubi_center_logo(fig: go.Figure, x: float, y: float = -0.12) -> None:
             yanchor="bottom",
         )
     )
+
+
+def format_fig(fig: go.Figure, show: bool=False, **kwargs) -> go.Figure:
+    """ Formats figure with UBI styling and logo.
+    **kwargs passed to add_ubi_center_logo.
+
+    :param fig: Plotly figure.
+    :type fig: go.Figure
+    :param show: Whether to show the figure.
+    :type show: bool
+    :return: Formatted plotly figure. Also shows it.
+    :rtype: go.Figure
+    """
+    add_ubi_center_logo(fig, **kwargs)
+    fig.update_xaxes(
+        title_font=dict(size=16, color="black"), tickfont={"size": 14}
+    )
+    fig.update_yaxes(
+        title_font=dict(size=16, color="black"), tickfont={"size": 14}
+    )
+    fig.update_layout(
+        hoverlabel_align="right",
+        font_family="Roboto",
+        title_font_size=20,
+        plot_bgcolor="white",
+        paper_bgcolor="white",
+        width=1000,
+        height=800,
+    )
+    if show:
+        fig.show(config=CONFIG)
+    return fig
