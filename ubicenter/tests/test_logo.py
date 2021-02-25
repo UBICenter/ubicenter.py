ab777bb23757541441df2686cf821be257f3bd63
Max Ghenis <mghenis@gmail.com>
2021-02-24 19:48:58 -0800

    Add test

--- ubicenter/tests/test_logo.py
+++ ubicenter/tests/test_logo.py
@@ -3,10 +3,13 @@ import plotly.express as px
 import ubicenter as ubic
 
 
-def test_logo():
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+
+
+def test_add_ubi_center_logo():
     ubic.add_ubi_center_logo(fig, 1)
 
 
-def 
+def test_format_fig():
+    ubic.format_fig(fig)
